3514dd88fa360fc30a479a7f567a32bc01264e0a
Brian Dunnette <brian@dunnette.us>
2015-01-31 10:07:30 -0600

    customizing models

--- client/views/sales/salesList.js
+++ client/views/sales/salesList.js
@@ -1,5 +1,9 @@
 Template.SalesList.helpers({
-
+  customerOptions: function () {
+    return People.find().map(function (c) {
+      return {label: c.name, value: c._id};
+    });
+  }
 });
 
 Template.SalesList.events({
