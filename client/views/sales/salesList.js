f07286f26f2c600bf21c61118c9e4db46685d1f5
Brian Dunnette <brian@dunnette.us>
2015-02-02 14:29:00 -0600

    moving sales options from template helpers to schema

--- client/views/sales/salesList.js
+++ client/views/sales/salesList.js
@@ -1,9 +1,5 @@
 Template.SalesList.helpers({
-  customerOptions: function () {
-    return People.find().map(function (c) {
-      return {label: c.name, value: c._id};
-    });
-  }
+  
 });
 
 Template.SalesList.events({
