5f9983e6c33a43a1ecd4e141089a48a7f042bd77
Brian Dunnette <brian@dunnette.us>
2015-07-26 20:05:04 -0500

    cleaning up sale template & admin

--- app/lib/admin-config.js
+++ app/lib/admin-config.js
@@ -1,6 +1,12 @@
 AdminConfig = {
+  name: 'till',
 collections:
 {
+Sales: {icon:'shopping-cart',tableColumns: [
+   { label: 'Customer', name: 'customer.name' },
+   { label: 'Subtotal', name: 'subtotal()' }
+  ]},
+People: {icon:'users'},
 Products: {icon:'desktop',tableColumns: [
    { label: 'Name', name: 'name' },
    { label: 'List Price', name: 'listPrice' },
@@ -10,8 +16,6 @@ Taxes: {icon:'usd',tableColumns: [
    { label: 'Tax Rate', name: 'taxRate' },
    { label: 'Start Date', name: 'startDate' },
    { label: 'End Date', name: 'endDate' }
-  ]},
-People: {icon:'users'},
-Sales: {icon:'shopping-cart'}
+  ]}
 }
 };
