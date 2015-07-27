2b0239ada7cf3fb82f126bec3f46dca664e7f2eb
Brian Dunnette <brian@dunnette.us>
2015-07-26 19:43:04 -0500

    admin icons & columns

--- app/lib/admin-config.js
+++ app/lib/admin-config.js
@@ -1,7 +1,17 @@
 AdminConfig = {
 collections:
 {
-Products: {},
-Taxes: {}
+Products: {icon:'desktop',tableColumns: [
+   { label: 'Name', name: 'name' },
+   { label: 'List Price', name: 'listPrice' },
+   { label: 'Taxable?', name: 'taxable' }
+  ]},
+Taxes: {icon:'usd',tableColumns: [
+   { label: 'Tax Rate', name: 'taxRate' },
+   { label: 'Start Date', name: 'startDate' },
+   { label: 'End Date', name: 'endDate' }
+  ]},
+People: {icon:'users'},
+Sales: {icon:'shopping-cart'}
 }
-}
+};
